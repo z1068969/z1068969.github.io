fb1b4fcacbfe60caea9c070ee64e204039795216
z1068969 <z1068969@gmail.com>
2024-10-11 14:11:24 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+window.addEventListener("scroll", setScrollVar)
+window.addEventListener("resize", setScrollVar)
+
+function setScrollVar(){
+	const htmlElement = document.documentElement
+	const percentOfScreenHeightScrolled = htmlElement.scrollTop / (htmlElement.clientHeight- 100)
+		console.log(Math.min(percentOfScreenHeightScrolled * 100, 100))
+	htmlElement.style.setProperty("--scroll", Math.min(percentOfScreenHeightScrolled * 100, 100))
+}
+
+setScrollVar()
